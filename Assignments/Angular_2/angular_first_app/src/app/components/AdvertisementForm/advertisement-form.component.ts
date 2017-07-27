508765d212e297b20bf2711190edceecb3843f5c
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-07-27 17:30:08 +0530

    Angular added

---
+++ Assignments/Angular_2/angular_first_app/src/app/components/AdvertisementForm/advertisement-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'advertisement-form',
+    templateUrl: './advertisement-form.html'
+})
+export class AdvertisementFormComponent {
+    name : string = 'Rutwik';
+    title : string = 'Post New Adv';
+}
