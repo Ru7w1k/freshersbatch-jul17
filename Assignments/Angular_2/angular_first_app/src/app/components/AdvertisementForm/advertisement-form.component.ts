3633c716e5c9139219ac58612dc837f4effdd1a2
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-07-28 10:28:36 +0530

    added more app

--- Assignments/Angular_2/angular_first_app/src/app/components/AdvertisementForm/advertisement-form.component.ts
+++ Assignments/Angular_2/angular_first_app/src/app/components/AdvertisementForm/advertisement-form.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
     templateUrl: './advertisement-form.html'
 })
 export class AdvertisementFormComponent {
-    name : string = 'Rutwik';
-    title : string = 'Post New Adv';
+
+    ads: Adv[] = [];
+    name: string = 'Rutwik';
+    title: string = 'Post New Adv';
+    categories: string[] = ['Furniture', 'Hardware', 'Mobile'];
+    
+
+    postAdv(title: string, name: string, category: string, desc: string, img:string) : void {
+        let adv = new Adv(title, name, category, desc, img);
+        this.ads.push(adv);
+    }
+}
+
+class Adv {
+    title: string;
+    name: string;
+    category: string;
+    description: string;
+    img: string;
+
+    constructor(title: string, name: string, category: string, description: string, img:string) {
+        this.title = title;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.img = img;
+    }
 }
---
+++ Assignments/Angular_2/angular_first_app/src/app/components/AdvertisementTable/advertisement-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'advertisement-table',
+    templateUrl: './advertisement-table.html'
+})
+
+export class AdvertisementTableComponent {
+    ads:
+
+}
